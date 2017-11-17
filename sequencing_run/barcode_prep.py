f564b80752d45dc590153eae642502c0ddf981ad
Matthew Mah <mmah@broadinstitute.org>
2017-11-17 14:46:26 -0500

    do not use barcode labels

--- sequencing_run/barcode_prep.py
+++ sequencing_run/barcode_prep.py
@@ -1,36 +1,47 @@
 from ssh_command import ssh_command
-from barcode_map import barcode_to_label, barcode_to_full_barcode_set
 
+# Find the set of barcodes used for a sequencing run 
+# Save a file in the run directory as input for the software pipeline
 def barcodes_used(sequencing_date_string, sequencing_run_name):
-	host = "mym11@login.rc.hms.harvard.edu"
 	queryForBarcodes = 'SELECT p5_barcode FROM sequenced_library WHERE sequencing_id="%s" AND length(p5_barcode) > 0 UNION SELECT p7_barcode FROM sequenced_library WHERE sequencing_id="%s" AND length(p7_barcode) > 0;' % (sequencing_run_name, sequencing_run_name)
-	command = "mysql devadna -N -e '%s'" % (queryForBarcodes)
+	
+	return _barcodes_used(sequencing_date_string, sequencing_run_name, queryForBarcodes, 'barcodes')
+	
+def _barcodes_used(sequencing_date_string, sequencing_run_name, query, extension):
+	host = "mym11@login.rc.hms.harvard.edu"
+	command = "mysql devadna -N -e '%s'" % (query)
 	ssh_result = ssh_command(host, command, False, True)
 	
-	barcodeSet = {}
+	barcodeSets = []
 	for line in ssh_result.stdout.readlines():
-		barcode = line.strip().upper() # uppercase only
-		barcodeSet[barcode] = 0
+		barcodeSets.append(line.strip().upper() ) # uppercase only
 	
+	barcodeSetForRun = set()
+	usedBarcodes = set()
 	# on first pass, populate sets with more than one barcode
-	barcodeMapForRun = {}
-	for barcode in barcodeSet:
-		if ':' in barcode:
-			label = barcode_to_label(barcode)
-			barcodeMapForRun[barcode] = label
+	for barcodeSet in barcodeSets:
+		if ':' in barcodeSet:
+			barcodeSetForRun.add(barcodeSet)
+			# each individual barcode will be considered as this set
+			individualBarcodes = barcodeSet.split(':')
+			for individualBarcode in individualBarcodes:
+				usedBarcodes.add(individualBarcode)
+				
 	# on second pass, add singleton barcodes
-	for barcode in barcodeSet:
-		if ':' not in barcode:
-			fullBarcodeSet = barcode_to_full_barcode_set(barcode)
-			if fullBarcodeSet not in barcodeMapForRun:
-				barcodeMapForRun[barcode] = barcode
+	for barcode in barcodeSets:
+		if (':' not in barcode) and (barcode not in usedBarcodes) and (barcode not in barcodeSetForRun):
+			barcodeSetForRun.add(barcode)
 	
-	barcodeLines = map(lambda barcode: "%s\t%s" % (barcode, barcodeMapForRun[barcode]), barcodeMapForRun)
+	# print the barcode twice because the format is [barcode label]
+	barcodeLines = map(lambda barcode: "%s\t%s" % (barcode, barcode), barcodeSetForRun)
 	barcodeFileTextOutput = '\n'.join(barcodeLines)
+	save_file_with_contents(barcodeFileTextOutput, sequencing_date_string, sequencing_run_name, extension, host)
+	#print (barcodeSetForRun)
+	return barcodeSetForRun
+
+# put a file in the run directory with the requested contents
+def save_file_with_contents(contents, sequencing_date_string, sequencing_run_name, extension, host):
 	
-	saveFileCommand = "echo '%s' > /home/mym11/pipeline/run/%s_%s.barcodes" % (barcodeFileTextOutput, sequencing_date_string, sequencing_run_name)
-	print (saveFileCommand)
+	saveFileCommand = "echo '%s' > /home/mym11/pipeline/run/%s_%s.%s" % (contents, sequencing_date_string, sequencing_run_name, extension)
+	#print (saveFileCommand)
 	ssh_command(host, saveFileCommand, False, True)
-#	print (output)
-#	for barcode in barcodeMapForRun:
-		#print("%s\t%s" % (barcode, barcodeMapForRun[barcode]) )
