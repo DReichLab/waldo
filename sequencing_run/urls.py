4829cd59ba37fb33e198c729812a09307553823d
Matthew Mah <mmah@broadinstitute.org>
2018-01-30 14:37:54 -0500

    Use regular expressions for url/path matching again

--- sequencing_run/urls.py
+++ sequencing_run/urls.py
@@ -3,8 +3,8 @@ from django.urls import path, re_path
 from . import views
 
 urlpatterns = [
-	path('updateRuns', views.updateSequencingRunList, name='Update sequencing runs'),
-	path('start', views.startScreeningAnalysis, name='Start Analysis'),
-	path('report_help', views.helpPage),
+	re_path(r'^updateRuns', views.updateSequencingRunList, name='Update sequencing runs'),
+	re_path(r'^start', views.startScreeningAnalysis, name='Start Analysis'),
+	re_path(r'^report_help', views.helpPage),
 	re_path(r'^$', views.screeningForm, name='Screening analysis'),
 ]
