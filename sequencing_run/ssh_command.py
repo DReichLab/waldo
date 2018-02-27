955a3eed204b80f835d99808f2a0a579b21b46f4
Matthew Mah <mmah@broadinstitute.org>
2018-02-27 16:14:31 -0500

    allow ssh_command to take objects to handle django management commands

--- sequencing_run/ssh_command.py
+++ sequencing_run/ssh_command.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python
 
+from __future__ import print_function
 import subprocess
 import sys
 
-def ssh_command(host, command, output_std=False, output_error=False):
+def ssh_command(host, command, output_std=None, output_error=None):
 	ssh_result = subprocess.Popen(["ssh", "%s" % host, command],
 						shell=False,
 						stdout=subprocess.PIPE,
@@ -11,13 +12,15 @@ def ssh_command(host, command, output_std=False, output_error=False):
 						universal_newlines=True)
 	
 	if output_std:
+		output = sys.stdout if output_std == True else output_std
 		lines = ssh_result.stdout.readlines()
 		for line in lines:
-			print (line)
+			print (line.strip(), file=output)
 			
 	if output_error:
+		output = sys.stderr if output_error == True else output_error
 		lines = ssh_result.stderr.readlines()
 		for line in lines:
-			sys.stderr.write (line)
+			print (line.strip(), file=output)
 	
 	return ssh_result
