6def78e74032356105ceed63b40a469d515acff6
Matthew Mah <mmah@broadinstitute.org>
2018-05-02 23:26:07 -0400

    command to copy illumina directory alone for a sequencing run in db

---
+++ sequencing_run/management/commands/copy_illumina_directory.py
@@ -0,0 +1,29 @@
+from django.core.management.base import BaseCommand, CommandError
+from django.conf import settings
+from django.utils import timezone
+import datetime
+from sequencing_run.models import SequencingRun, SequencingAnalysisRun
+from sequencing_run.analysis import copy_illumina_directory
+
+class Command(BaseCommand):
+	help = 'recopy illumina directory for a sequencing run'
+	
+	def add_arguments(self, parser):
+		parser.add_argument('--date_string', nargs=1)
+		parser.add_argument('--name', nargs=1)
+		
+	def handle(self, *args, **options):
+		date_string = options['date_string'][0]
+		name = options['name'][0]
+		date = datetime.datetime.strptime(date_string, "%Y%m%d").date()
+		
+		run_entry = SequencingAnalysisRun.objects.get(
+			name = name,
+			sequencing_date = date
+		)
+
+		destination_directory = date_string + '_' + name
+		scratch_illumina_parent_path = settings.SCRATCH_PARENT_DIRECTORY + "/" + destination_directory
+		
+		illumina_directory = run_entry.sequencing_run.illumina_directory
+		copy_illumina_directory(illumina_directory, scratch_illumina_parent_path)
