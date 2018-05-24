a295fc2c1163a4291753b422ad38536afc8c32ab
Matthew Mah <mmah@broadinstitute.org>
2018-05-24 13:39:29 -0400

    make saving files in run directory more flexible

--- sequencing_run/ssh_command.py
+++ sequencing_run/ssh_command.py
@@ -31,12 +31,16 @@ def ssh_command(host, command, output_std=None, output_error=None):
 
 # put a file in the run directory with the requested contents
 def save_file_with_contents(contents, sequencing_date_string, sequencing_run_name, extension, host):
+	filename = "{}_{}.{}".format(sequencing_date_string, sequencing_run_name, extension)
+	directory = "{}/{}_{}".format(settings.RUN_FILES_DIRECTORY, sequencing_date_string, sequencing_run_name)
+	save_file_base(contents, directory, filename, host)
+
+def save_file_base(contents, directory, filename, host):
 	# using echo limits the file size
 	# save a temporary local file using a temporary directory, then copy this to run directory
 	with tempfile.TemporaryDirectory() as temp_directory:
-		filename = "{}_{}.{}".format(sequencing_date_string, sequencing_run_name, extension)
 		filename_full_path = os.path.join(temp_directory, filename)
 		with open(filename_full_path, 'w') as f:
 			f.write(contents)
-		destination = "{0}:{1}/{2}_{3}/{2}_{3}.{4}".format(host, settings.RUN_FILES_DIRECTORY, sequencing_date_string, sequencing_run_name, extension)
+		destination = "{0}:{1}/{2}".format(host, directory, filename)
 		subprocess.check_output(['scp', filename_full_path, destination])
