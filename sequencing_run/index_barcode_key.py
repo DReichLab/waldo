7ca791ce0de841b76d632465f57829a137e9880f
Matthew Mah <mmah@broadinstitute.org>
2018-07-26 10:00:41 -0400

    IndexBarcodeKey class

---
+++ sequencing_run/index_barcode_key.py
@@ -0,0 +1,38 @@
+class IndexBarcodeKey():
+	BARCODE_DELIMITER = ':'
+	
+	def __init__(self, i5, i7, p5='', p7=''):
+		self.i5 = i5 
+		self.i7 = i7
+		
+		if len(self.i5) != len(self.i7):
+			raise ValueError('mismatch in index lengths')
+		
+		self.p5 = p5 
+		self.p7 = p7
+		
+		if len(self.p5) != len(self.p7):
+			raise ValueError('mismatch in barcode lengths')
+		for barcode_p5_element in self.p5.split(IndexBarcodeKey.BARCODE_DELIMITER):
+			for barcode_p7_element in self.p7.split(IndexBarcodeKey.BARCODE_DELIMITER):
+				if len(barcode_p5_element) != len(barcode_p7_element):
+					raise ValueError('mismatch in barcode lengths')
+				
+	def __str__(self):
+		return '{}_{}_{}_{}'.format(self.i5, self.i7, self.p5, self.p7)
+	
+	def barcode_subset(barcode_A, barcode_B):
+		A = barcode_A.split(IndexBarcodeKey.BARCODE_DELIMITER)
+		B = barcode_B.split(IndexBarcodeKey.BARCODE_DELIMITER)
+		barcodeLengthA = len(A[0])
+		barcodeLengthB = len(B[0])
+		if barcodeLengthA != barcodeLengthB:
+			return False
+		for barcode in A:
+			if barcode not in B:
+				return False;
+		return True;
+	
+	def maps_to(self, other):
+		return (self.i5 == other.i5 and self.i7 == other.i7
+			and IndexBarcodeKey.barcode_subset(self.p5, other.p5) and IndexBarcodeKey.barcode_subset(self.p7, other.p7))
---
+++ sequencing_run/test_index_barcode_key.py
@@ -0,0 +1,50 @@
+from django.test import TestCase
+
+from django.conf import settings
+
+from .index_barcode_key import IndexBarcodeKey
+
+class IndexBarcodeKeyTest(TestCase):
+	def test_no_barcodes(self):
+		i5 = 'AGGTATT'
+		i7 = 'GCTTCAG'
+		key = IndexBarcodeKey(i5, i7)
+		
+		self.assertEqual(i5, key.i5)
+		self.assertEqual(i7, key.i7)
+		self.assertEqual('', key.p5)
+		self.assertEqual('', key.p7)
+		self.assertEqual('AGGTATT_GCTTCAG__', key.__str__())
+		
+	def test_37_1_Q18_Q21(self):
+		i5 = 'GCCATAG'
+		i7 = 'TCGCAGG'
+		p5 = 'ACGGTCT:CGTTAGA:GTAACTC:TACCGAG'
+		p7 = 'AGTCACG:CTAGCGT:GACTGTA:TCGATAC'
+		
+		key = IndexBarcodeKey(i5, i7, p5, p7)
+		
+		self.assertEqual(i5, key.i5)
+		self.assertEqual(i7, key.i7)
+		self.assertEqual(p5, key.p5)
+		self.assertEqual(p7, key.p7)
+		self.assertEqual('GCCATAG_TCGCAGG_ACGGTCT:CGTTAGA:GTAACTC:TACCGAG_AGTCACG:CTAGCGT:GACTGTA:TCGATAC', key.__str__())
+		
+		index_only_key = IndexBarcodeKey(i5, i7)
+		self.assertFalse(key.maps_to(index_only_key))
+		self.assertFalse(index_only_key.maps_to(key))
+		
+	def test_positive_control(self):
+		i5 = 'AGGTATT'
+		i7 = 'GCTTCAG'
+		
+		p5 = 'GGTATCG:TTACAGT:AACGCTA:CCGTGAC'
+		p7 = p5
+		key_from_data = IndexBarcodeKey(i5, i7, p5, p7)
+		
+		p5_sheet = 'TTACAGT'
+		p7_sheet = 'CCGTGAC'
+		key_from_sheet = IndexBarcodeKey(i5, i7, p5_sheet, p7_sheet)
+		
+		self.assertTrue(key_from_sheet.maps_to(key_from_data))
+		self.assertFalse(key_from_data.maps_to(key_from_sheet))
