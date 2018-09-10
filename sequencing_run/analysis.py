2a3e5580420a78eb5fd886381f4c06b18250b078
Matthew Mah <mmah@broadinstitute.org>
2018-09-10 11:04:26 -0400

    need to check whether cromwell jobs are running in states that are demultiplexing or later

--- sequencing_run/analysis.py
+++ sequencing_run/analysis.py
@@ -165,7 +165,7 @@ def query_job_status():
 		
 	# iterate over running sequencing analysis runs
 	# if the slurm job is not present
-	expectedRunningJobs = SequencingAnalysisRun.objects.filter(processing_state__gte=SequencingAnalysisRun.RUNNING_ANALYSIS).exclude(processing_state__gte=SequencingAnalysisRun.FINISHED)
+	expectedRunningJobs = SequencingAnalysisRun.objects.filter(processing_state__gte=SequencingAnalysisRun.DEMULTIPLEXING).exclude(processing_state__gte=SequencingAnalysisRun.FINISHED)
 	for expectedRunningJob in expectedRunningJobs:
 		# query for sacct info and check for COMPLETED state
 		#sacct -j 5790362 -o "JobID,State"
