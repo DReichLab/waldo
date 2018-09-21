2954582f2ce3a30cc2145cd20a0721545f958dba
Matthew Mah <mmah@broadinstitute.org>
2018-09-21 14:42:16 -0400

    Move secret key to unversioned file, change key.

--- screening/settings.py
+++ screening/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
+import secret_settings.py
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -19,11 +20,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '&+8k1eb78qcr!1u^1b#u6_nu%r66-*om&f208u+ve1%hdzak&)'
-
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['localhost', 'screening.reichdna.hms.harvard.edu', 'testing.reichdna.hms.harvard.edu']
 
