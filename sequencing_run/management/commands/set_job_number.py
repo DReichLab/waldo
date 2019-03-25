9a1101241eae2579ea7b6fa2a5dc73366b25738e
Matthew Mah <mmah@broadinstitute.org>
2019-03-25 11:44:37 -0400

    allow processing state to be set with job number

--- sequencing_run/management/commands/set_job_number.py
+++ sequencing_run/management/commands/set_job_number.py
@@ -8,19 +8,22 @@ class Command(BaseCommand):
 	help = 'reset the slurm job number for an analysis run'
 	
 	def add_arguments(self, parser):
-		parser.add_argument('--date_string', nargs=1)
-		parser.add_argument('--name', nargs=1)
-		parser.add_argument('--job', nargs=1)
+		parser.add_argument('--date_string', required=True)
+		parser.add_argument('--name', required=True)
+		parser.add_argument('--job', required=True, type=int)
+		parser.add_argument('-p', '--processing_state', type=int)
 		
 	def handle(self, *args, **options):
-		date_string = options['date_string'][0]
-		name = options['name'][0]
+		date_string = options['date_string']
+		name = options['name']
 		date = datetime.datetime.strptime(date_string, "%Y%m%d").date()
-		job_number = int(options['job'][0])
+		job_number = options['job']
 		
 		run_entry = SequencingAnalysisRun.objects.get(
 			name = name,
 			sequencing_date = date
 		)
 		run_entry.slurm_job_number = job_number
+		if options['processing_state']:
+			run_entry.processing_state = options['processing_state']
 		run_entry.save()
