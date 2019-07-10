788c7a818f8a138f7a9bf5c565586e0e7bbb4e66
Matthew Mah <mmah@broadinstitute.org>
2019-07-10 16:19:56 -0400

    Do not display Broad illumina directories in interface because wetlab does not access them directly

--- sequencing_run/forms.py
+++ sequencing_run/forms.py
@@ -1,13 +1,17 @@
 from django import forms
+from django.db.models.functions import Length
 
 from .models import SequencingRun, SequencingRunID
 from .models import Flowcell
 
+# only display sequencing run (illumina directory strings) with length > 25
+# These are the NextSeq runs on the Genetics file server, as opposed to the fake Broad entries, which are not 
+
 class StatusForm(forms.Form):
-	illumina_directory = forms.ModelChoiceField(queryset=SequencingRun.objects.all().order_by('-illumina_directory'), label='Illumina directory', empty_label='Illumina directory', to_field_name='illumina_directory')
+	illumina_directory = forms.ModelChoiceField(queryset=SequencingRun.objects.annotate(text_len=Length('illumina_directory')).filter(text_len__gt=25).order_by('-illumina_directory'), label='Illumina directory', empty_label='Illumina directory', to_field_name='illumina_directory')
 
 class AnalysisForm(forms.Form):
-	illumina_directory = forms.ModelChoiceField(queryset=SequencingRun.objects.all().order_by('-illumina_directory'), label='Illumina directory', empty_label='Illumina directory', to_field_name='illumina_directory')
+	illumina_directory = forms.ModelChoiceField(queryset=SequencingRun.objects.annotate(text_len=Length('illumina_directory')).filter(text_len__gt=25).order_by('-illumina_directory'), label='Illumina directory', empty_label='Illumina directory', to_field_name='illumina_directory')
 	name1 = forms.ModelChoiceField(queryset=SequencingRunID.objects.all().order_by('-order'), label="sequencing run name", )
 	name2 = forms.ModelChoiceField(queryset=SequencingRunID.objects.all().order_by('-order'), label="sequencing run name (optional)", required=False)
 	name3 = forms.ModelChoiceField(queryset=SequencingRunID.objects.all().order_by('-order'), label="sequencing run name (optional)", required=False)
