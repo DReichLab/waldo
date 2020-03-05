a26e1f136b722ec94669c324f4badc6fb5852e9e
Matthew Mah <mmah@broadinstitute.org>
2020-03-05 15:35:24 -0500

    Working on query by individual ID

--- samples/views.py
+++ samples/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
 
+from django.http import HttpResponse
+
+from .forms import IndividualForm
+
 # Create your views here.
+
+def query(request):
+	if request.method == 'POST':
+		form = IndividualForm(request.POST)
+		if form.is_valid():
+			individual_id = form.cleaned_data['individual_id']
+			
+			return HttpResponse("This will be a query result for {:d}.".format(individual_id))
+		else:
+			return HttpResponse("Invalid form")
+	else:
+		form = IndividualForm()
+		return render(request, 'samples/individuals.html', {'form': form})
+
+def hello(request):
+	return HttpResponse("Hello, world.")
--- screening/urls.py
+++ screening/urls.py
@@ -20,5 +20,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
 	path('sequencing_run/', include('sequencing_run.urls')),
+	path('samples/', include('samples.urls')),
     path('admin/', admin.site.urls),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
