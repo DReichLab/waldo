5e276bfd9e9c9b41af11516fbde46728a28a79b5
Matthew Mah <mmah@broadinstitute.org>
2020-06-29 14:55:31 -0400

    Rebecca's views for extract and powder calculations

--- create_postresql_views.sql
+++ create_postresql_views.sql
@@ -7,3 +7,21 @@ CREATE OR REPLACE VIEW sequencing_run_shotgunanalysis_view AS
 	SELECT *,
 	(reads_mapped_hg19::decimal / raw_sequences) AS endogenous_fraction_from_raw
 	FROM sequencing_run_shotgunanalysis;
+
+CREATE OR REPLACE VIEW extract_used_total AS
+	SELECT samples_library.extract_id_id,
+	sum(samples_library.ul_extract_used) AS total_extract
+	FROM samples_library
+	GROUP BY samples_library.extract_id_id;
+
+CREATE OR REPLACE VIEW lysate_used_total AS
+	SELECT samples_extract.lysate_id_id,
+	sum(samples_extract.lysis_volume_extracted) AS total_lysate_extracted
+	FROM samples_extract
+	GROUP BY samples_extract.lysate_id_id;
+
+CREATE OR REPLACE VIEW powder_used_total AS
+	SELECT samples_lysate.powder_sample_id,
+	sum(samples_lysate.powder_used_mg) AS total_powder_lysed
+	FROM samples_lysate
+	GROUP BY samples_lysate.powder_sample_id;
--- drop_postgresql_views.sql
+++ drop_postgresql_views.sql
@@ -1,2 +1,5 @@
 DROP VIEW IF EXISTS samples_view;
 DROP VIEW IF EXISTS sequencing_run_shotgunanalysis_view;
+DROP VIEW IF EXISTS extract_used_total;
+DROP VIEW IF EXISTS lysate_used_total;
+DROP VIEW IF EXISTS powder_used_total;
