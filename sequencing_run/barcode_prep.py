efd317b287eb3b1f494679b07d482a70db8200e7
Matthew Mah <mmah@broadinstitute.org>
2020-11-18 10:54:35 -0500

    fix explicit barcode additions

--- sequencing_run/barcode_prep.py
+++ sequencing_run/barcode_prep.py
@@ -11,7 +11,7 @@ def barcodes_set(sequencing_date_string, combined_sequencing_run_name, sequencin
 	where_clauses = " OR ".join(['sequencing_id="{}"'.format(name) for name in sequencing_run_names])
 	queryForBarcodes = 'SELECT UPPER(p5_barcode) FROM sequenced_library WHERE ({0}) AND length(p5_barcode) > 0 UNION SELECT UPPER(p7_barcode) FROM sequenced_library WHERE ({0}) AND length(p7_barcode) > 0 UNION SELECT barcode FROM barcode WHERE barcode_id LIKE "Q%";'.format(where_clauses)
 	
-	return _barcodes_set(sequencing_date_string, combined_sequencing_run_name, queryForBarcodes, 'barcodes', explicit_additions = [])
+	return _barcodes_set(sequencing_date_string, combined_sequencing_run_name, queryForBarcodes, 'barcodes', explicit_additions)
 
 
 # i5 indices from sample sheet plus standard Reich Lab i5 indices (1-48) and the shotgun indices (49-53)
@@ -21,7 +21,7 @@ def i5_set(sequencing_date_string, combined_sequencing_run_name, sequencing_run_
 	where_clauses = " OR ".join(['sequencing_id="{}"'.format(name) for name in sequencing_run_names])
 	queryForBarcodes = 'SELECT UPPER(p5_index) FROM sequenced_library WHERE ({}) AND length(p5_index) > 3 UNION SELECT UPPER(sequence) FROM p5_index WHERE p5_index_key BETWEEN 1 AND 53 UNION SELECT UPPER(p5_index_seq) FROM new_p5_index;'.format(where_clauses)
 	
-	return _barcodes_set(sequencing_date_string, combined_sequencing_run_name, queryForBarcodes, 'i5', explicit_additions = [])
+	return _barcodes_set(sequencing_date_string, combined_sequencing_run_name, queryForBarcodes, 'i5', explicit_additions)
 
 # i7 indices from sample sheet plus standard Reich Lab i7 indices (1-96)
 # eliminate '..' entries by requiring length > 3
@@ -30,7 +30,7 @@ def i7_set(sequencing_date_string, combined_sequencing_run_name, sequencing_run_
 	where_clauses = " OR ".join(['sequencing_id="{}"'.format(name) for name in sequencing_run_names])
 	queryForBarcodes = 'SELECT UPPER(p7_index) FROM sequenced_library WHERE ({}) AND length(p7_index) > 3 UNION SELECT UPPER(sequence) FROM p7_index WHERE p7_index_key BETWEEN 1 AND 96 UNION SELECT UPPER(p7_index_seq) FROM new_p7_index;'.format(where_clauses)
 	
-	return _barcodes_set(sequencing_date_string, combined_sequencing_run_name, queryForBarcodes, 'i7', explicit_additions = [])
+	return _barcodes_set(sequencing_date_string, combined_sequencing_run_name, queryForBarcodes, 'i7', explicit_additions)
 
 # run a database query with the 
 def _barcodes_set(sequencing_date_string, combined_sequencing_run_name, query, extension, explicit_additions = []):
