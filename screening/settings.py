af7dbc7524c5a1d902b9f3a8f66b1cf34022a103
Matthew Mah <mmah@broadinstitute.org>
2021-03-11 10:37:58 -0500

    password validation in settings

--- screening/settings.py
+++ screening/settings.py
@@ -80,6 +80,24 @@ DATABASES = {
         'NAME': 'adna2',
     }
 }
+	
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'OPTIONS': {
+            'min_length': 10,
+        }
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
 
 
 # Internationalization
