f6ee23bee2983fd1ef9eac6a48793a0313af2d82
Adam Micco <adam_micco@hms.harvard.edu>
2022-01-21 14:40:43 -0500

    add support for adna2 based index barcode key lookup to index_barcode_keys_used.py

--- sequencing_run/management/commands/index_barcode_keys_used.py
+++ sequencing_run/management/commands/index_barcode_keys_used.py
@@ -2,7 +2,7 @@ from django.core.management.base import BaseCommand, CommandError
 from django.conf import settings
 import datetime
 from sequencing_run.ssh_command import ssh_command
-from sequencing_run.analysis import index_barcode_keys_used, barcodes_set, i5_set, i7_set
+from sequencing_run.analysis import adna2_index_barcode_keys_used, index_barcode_keys_used, barcodes_set, i5_set, i7_set
 from sequencing_run.models import SequencingAnalysisRun
 
 class Command(BaseCommand):
@@ -12,17 +12,23 @@ class Command(BaseCommand):
 		parser.add_argument('--date_string', required=True)
 		parser.add_argument('--name', required=True)
 		parser.add_argument('--ignore_barcodes', action='store_true')
+		parser.add_argument('--mysql_ibk', action='store_true')
 		
 	def handle(self, *args, **options):
 		date_string = options['date_string']
 		name = options['name']
 		date = datetime.datetime.strptime(date_string, "%Y%m%d").date()
+		ignore_barcodes = options['ignore_barcodes']
+		mysql_ibk = options['mysql_ibk']
 		
 		analysis_run = SequencingAnalysisRun.objects.get(name=name, sequencing_date=date)
 		sequencing_run_ids = list(analysis_run.sample_set_names.all())
 		separate_sequencing_run_names = [run.name for run in sequencing_run_ids]
 		#print(names)
-		index_barcode_keys_used(date_string, name, separate_sequencing_run_names, ignore_barcodes)
+		if mysql_ibk:
+			index_barcode_keys_used(date_string, name, separate_sequencing_run_names, ignore_barcodes)
+		else:
+			adna2_index_barcode_keys_used(date_string, name, separate_sequencing_run_names)
 		
 		# create barcode file too
 		barcodes_set(date_string, name, separate_sequencing_run_names)
