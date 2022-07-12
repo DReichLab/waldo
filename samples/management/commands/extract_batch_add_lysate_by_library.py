e7ee3a9d970250cb1d8546e3c2da91c0e40ca94d
Matthew Mah <mmah@broadinstitute.org>
2022-07-12 16:37:58 -0400

    Loading Crowd libraries(lysates) for extract batches is optional to allow creation of controls alone

--- samples/management/commands/extract_batch_add_lysate_by_library.py
+++ samples/management/commands/extract_batch_add_lysate_by_library.py
@@ -10,7 +10,7 @@ class Command(BaseCommand):
 		parser.add_argument('--controls', action='store_true', help="Populate lysate batch's control layout")
 		parser.add_argument('user')
 		parser.add_argument("extract_batch_name")
-		parser.add_argument('parameter_file')
+		parser.add_argument('-f', '--parameter_file')
 		
 	def handle(self, *args, **options):
 		plate = ExtractionBatch.objects.get(batch_name=options['extract_batch_name'])
@@ -24,21 +24,22 @@ class Command(BaseCommand):
 			plate.set_controls(user)
 		
 		# This file contains library id and position, one per line
-		with open(options['parameter_file']) as f:
-			for line in f:
-				fields = re.split('\t', line)
-				library_id = fields[0].strip()
-				plate_location = fields[1].strip()
-				
-				self.stdout.write(f"{plate.batch_name}\t{library_id}\t{plate_location}")
-				
-				#with transaction.atomic():
-				if library_id.startswith('S'):
-					row = plate_location[0]
-					column = int(plate_location[1:])
+		if options['parameter_file']:
+			with open(options['parameter_file']) as f:
+				for line in f:
+					fields = re.split('\t', line)
+					library_id = fields[0].strip()
+					plate_location = fields[1].strip()
 					
-					library = Library.objects.get(reich_lab_library_id=library_id)
-					lysate = library.extract.lysate
-					plate.add_lysate(lysate, row, column, user)
-				else:
-					self.stderr.write(f'skipping {library_id}')
+					self.stdout.write(f"{plate.batch_name}\t{library_id}\t{plate_location}")
+					
+					#with transaction.atomic():
+					if library_id.startswith('S'):
+						row = plate_location[0]
+						column = int(plate_location[1:])
+						
+						library = Library.objects.get(reich_lab_library_id=library_id)
+						lysate = library.extract.lysate
+						plate.add_lysate(lysate, row, column, user)
+					else:
+						self.stderr.write(f'skipping {library_id}')
