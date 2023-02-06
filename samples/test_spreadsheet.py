ea6c1e0eaff2dff2e2c74425ab74743ce94556ac
Matthew Mah <mmah@broadinstitute.org>
2023-02-06 14:28:37 -0500

    Handle spreadsheets both from web submission and command line. Add spreadsheet tests.

--- samples/test_spreadsheet.py
+++ samples/test_spreadsheet.py
@@ -4,6 +4,8 @@ from django.core.exceptions import ValidationError
 from .spreadsheet import *
 
 class SpreadsheetTest(SimpleTestCase):
+	spreadsheet_file = 'samples/test/spreadsheet_example'
+
 	def test_header_appending_simple(self):
 		header = 'a'
 		altered_header = spreadsheet_header_read_only(header)
@@ -21,3 +23,17 @@ class SpreadsheetTest(SimpleTestCase):
 		altered_headers = spreadsheet_headers_read_only(headers)
 		for header in altered_headers:
 			self.assertEquals(s_read_only, header)
+
+	def test_exact(self):
+		with open(self.spreadsheet_file) as f:
+			headers, data_rows = spreadsheet_headers_and_data_row_fields(f)
+
+			row1 = data_rows[0]
+			self.assertEqual('1', get_spreadsheet_value(headers, row1, 'a') )
+			self.assertEqual('2', get_spreadsheet_value(headers, row1, 'b') )
+			self.assertEqual('3', get_spreadsheet_value(headers, row1, 'c-') )
+
+			row2 = data_rows[1]
+			self.assertEqual('10', get_spreadsheet_value(headers, row2, 'a') )
+			self.assertEqual('20', get_spreadsheet_value(headers, row2, 'b') )
+			self.assertEqual('30', get_spreadsheet_value(headers, row2, 'c-') )
