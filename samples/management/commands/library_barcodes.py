fb8bbf829e2e8ddbf5dbc5e09885a6b5b9bcb1f0
Matthew Mah <mmah@broadinstitute.org>
2023-06-16 16:33:40 -0400

    Print barcodes for multiple libraries for one invocation

--- samples/management/commands/library_barcodes.py
+++ samples/management/commands/library_barcodes.py
@@ -7,14 +7,15 @@ class Command(BaseCommand):
 	help = 'Inspect barcodes for a library'
 	
 	def add_arguments(self, parser):
-		parser.add_argument('library_id', help='Reich Lab library ID')
+		parser.add_argument('library_id', nargs='+', help='Reich Lab library ID')
 		
 	def handle(self, *args, **options):
-		library_id = options['library_id']
-		library = Library.objects.get(reich_lab_library_id=library_id)
+		library_ids = options['library_id']
+		for library_id in library_ids:
+			library = Library.objects.get(reich_lab_library_id=library_id)
 
-		p5_label = get_value(library, 'p5_barcode', 'label')
-		p5_sequence = get_value(library, 'p5_barcode', 'sequence')
-		p7_label = get_value(library, 'p7_barcode', 'label')
-		p7_sequence = get_value(library, 'p7_barcode', 'sequence')
-		self.stdout.write('\t'.join([library_id, p5_label, p5_sequence, p7_label, p7_sequence]))
+			p5_label = get_value(library, 'p5_barcode', 'label')
+			p5_sequence = get_value(library, 'p5_barcode', 'sequence')
+			p7_label = get_value(library, 'p7_barcode', 'label')
+			p7_sequence = get_value(library, 'p7_barcode', 'sequence')
+			self.stdout.write('\t'.join([library_id, p5_label, p5_sequence, p7_label, p7_sequence]))
