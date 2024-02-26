f001b462132133873e4579b654edfa1cef5546ba
Matthew Mah <mmah@broadinstitute.org>
2024-02-26 17:21:52 -0500

    test sample parsing for library IDs

--- samples/test_sample_parse.py
+++ samples/test_sample_parse.py
@@ -30,3 +30,20 @@ class ParseSampleString(SimpleTestCase):
 		with self.assertRaises(ValueError):
 			s = 'xyz'
 			sample_number, control = parse_sample_string(s)
+
+	def test_library_string_control(self):
+		s = 'S12345a.Y1.E1.L1'
+		sample_number, control = parse_sample_string(s, False)
+		self.assertEqual(12345, sample_number)
+		self.assertEqual('a', control)
+
+	def test_library_string(self):
+		s = 'S54321.Y1.E1.L1'
+		sample_number, control = parse_sample_string(s, False)
+		self.assertEqual(54321, sample_number)
+		self.assertEqual('', control)
+
+	def test_library_string_full_fail(self):
+		s = 'S12345a.Y1.E1.L1'
+		with self.assertRaises(ValueError):
+			sample_number, control = parse_sample_string(s, True)
