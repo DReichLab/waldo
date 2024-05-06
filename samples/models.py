fbd2dda1b24ccea95e070e89415ce9de60272264
Matthew Mah <mmah@broadinstitute.org>
2024-05-06 10:38:34 -0400

    use powder value from lysate layout elements

--- samples/models.py
+++ samples/models.py
@@ -801,7 +801,7 @@ def create_lysate(lysate_layout_element, lysate_batch, user):
 		# library negatives have no lysis volume
 		is_library_negative = (lysate_layout_element.control_type is not None) and (lysate_layout_element.control_type.control_type == LIBRARY_NEGATIVE)
 		total_volume_produced = lysate_batch.protocol.total_lysis_volume if not is_library_negative else 0
-		powder_used_mg = powder_sample.powder_for_extract if powder_sample else 0
+		powder_used_mg = get_value(lysate_layout_element, 'powder_used_mg', 0)
 		lysate = Lysate(lysate_id=lysate_id,
 					reich_lab_lysate_number=next_lysate_number,
 					powder_sample=powder_sample,
