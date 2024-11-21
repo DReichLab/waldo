9290cf929bef289650efc2bb6ef3f7194c6f90ed
Matthew Mah <mmah@broadinstitute.org>
2024-11-21 14:13:52 -0500

    file input for library batch extract additions

--- samples/management/commands/library_batch_add_extract.py
+++ samples/management/commands/library_batch_add_extract.py
@@ -2,23 +2,30 @@ from django.core.management.base import BaseCommand, CommandError
 from django.conf import settings
 from django.db import transaction
 
-from samples.models import LibraryBatch, LibraryBatchLayout, get_value, Extract
+import re
+
+from samples.models import LibraryBatch, Extract
 
 class Command(BaseCommand):
-	help = "Remove the extract assigned to a well and the associated library"
+	help = "Add extract to a library batch well for each line in file"
 	
 	def add_arguments(self, parser):
 		parser.add_argument('library_batch')
-		parser.add_argument('row')
-		parser.add_argument('column', type=int)
-		parser.add_argument('extract_id')
+		parser.add_argument('-f', '--extracts_file', help='File containing two columns: position (B8) and extract id string', required=True)
 		
 	def handle(self, *args, **options):
+		library_batch = LibraryBatch.objects.get(name=options['library_batch'])
 		with transaction.atomic():
-			library_batch = LibraryBatch.objects.get(name=options['library_batch'])
-			extract = Extract.objects.get(extract_id=options['extract_id'])
-			row = options['row']
-			column = options['column']
+			with open(options['extracts_file']) as f:
+				for line in f:
+					fields = line.split()
+					position = fields[0]
+					row = position[0]
+					column = int(position[1:])
+					extract_id = fields[1]
 
-			self.stdout.write(extract.extract_id)
-			library_batch.assign_extract(extract, row, column)
+					extract = Extract.objects.get(extract_id=extract_id)
+
+					self.stdout.write(extract.extract_id)
+					library_batch.assign_extract(extract, row, column)
+			library_batch.clean()
