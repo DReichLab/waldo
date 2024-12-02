c912738c1c3a3af9c28f50b26f26751f0aeb76a6
Matthew Mah <mmah@broadinstitute.org>
2024-12-02 15:32:10 -0500

    another instance of sample_control default

--- samples/views.py
+++ samples/views.py
@@ -537,7 +537,7 @@ def sample(request):
 		form = SampleImageForm(request.POST, request.FILES)
 		if form.is_valid():
 			reich_lab_sample_number = reich_lab_sample_number_from_string(request.GET['sample'])
-			sample_control_letter = request.GET['sample_control']
+			sample_control_letter = request.GET.get('sample_control', '')
 			print(reich_lab_sample_number)
 			photo = request.FILES.get('photo')
 			label = form.cleaned_data['image_type']
