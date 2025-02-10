c8a7767f4d9559afbbb130b4fd8d154dc48affb8
Matthew Mah <mmah@broadinstitute.org>
2025-02-10 11:44:21 -0500

    remove double sort

--- samples/views.py
+++ samples/views.py
@@ -609,7 +609,7 @@ def sample_summary(request):
 		# apparently it's not possible to filter a queryset using a method
 		powder_samples = PowderSample.objects.filter(sample=sample).order_by('powder_sample_id')
 		lysate_layouts = LysateBatchLayout.objects.filter(Q(powder_sample__sample=sample) | Q(lysate__sample=sample) | Q(lysate__powder_sample__sample=sample)).distinct().select_related('lysate').order_by('lysate__reich_lab_lysate_number')
-		extract_layouts = ExtractionBatchLayout.objects.filter(Q(extract__sample=sample) | Q(extract__lysate__powder_sample__sample=sample) | Q(extract__lysate__sample=sample)).distinct().select_related('extract').order_by('lysate__sample__reich_lab_id', 'lysate__reich_lab_lysate_number', 'lysate__reich_lab_lysate_number', 'extract__reich_lab_extract_number')
+		extract_layouts = ExtractionBatchLayout.objects.filter(Q(extract__sample=sample) | Q(extract__lysate__powder_sample__sample=sample) | Q(extract__lysate__sample=sample)).distinct().select_related('extract').order_by('lysate__sample__reich_lab_id', 'lysate__reich_lab_lysate_number', 'extract__reich_lab_extract_number')
 		extracts = [layout.extract for layout in extract_layouts.all()]
 		# 
 		library_layouts = LibraryBatchLayout.objects.filter(Q(library__sample=sample) | Q(library__extract__in=extracts) ).distinct().select_related('library').order_by('library__extract__sample__reich_lab_id', 'library__extract__lysate__reich_lab_lysate_number', 'library__extract__reich_lab_extract_number', 'library__reich_lab_library_number')
