6d6b3c6748260ec6c0a139b43fd02b049c4f104c
Matthew Mah <mmah@broadinstitute.org>
2025-02-21 17:47:38 -0500

    Do not HTML form require P7 offset because this causes problems if protocol has been incorrectly assigned as DS when it should be SS. #156.

--- samples/forms.py
+++ samples/forms.py
@@ -545,7 +545,7 @@ class LibraryBatchForm(UserModelForm):
 			if self.instance.protocol:
 				self.fields['protocol'].queryset = LibraryProtocol.objects.filter(Q(active=True) | Q(id=self.instance.protocol.id) ).order_by('-start_date')
 				if self.instance.protocol.library_type == 'ds':
-					self.fields['p7_offset'].required = True
+					# self.fields['p7_offset'].required = True
 					# to keep offset consistent with libraries, only allow changing offset when batch is open
 					self.fields['p7_offset'].disabled = (self.instance.status != LibraryBatch.OPEN)
 			if self.instance.control_set:
