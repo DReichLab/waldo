5234a040748242a5e8ce81d65d2141ab319f149d
Matthew Mah <mmah@broadinstitute.org>
2025-03-21 15:20:21 -0400

    allow removing libraries from capture/shotgun batch with delete flag to add command

--- samples/management/commands/capture_or_shotgun_plate_add.py
+++ samples/management/commands/capture_or_shotgun_plate_add.py
@@ -1,6 +1,6 @@
 from django.core.management.base import BaseCommand, CommandError
 from django.db import transaction
-from samples.models import CaptureOrShotgunPlate, WetLabStaff
+from samples.models import CaptureOrShotgunPlate, WetLabStaff, get_wetlab_staff, CaptureLayout
 
 class Command(BaseCommand):
 	help = 'Add a single library or control identified by Reich Lab string to a shotgun or capture plate'
@@ -10,7 +10,8 @@ class Command(BaseCommand):
 		parser.add_argument("library_id", help='Reich lab library indentifier or control string')
 		parser.add_argument('row')
 		parser.add_argument('column', type=int)
-		parser.add_argument('user', help='Wetlab user first name')
+		parser.add_argument('-u', '--user', nargs='+', required=True, help='Wetlab Staff name or username')
+		parser.add_argument('-d', '--delete', action='store_true', help='Remove this library from position instead of adding')
 		
 	def handle(self, *args, **options):
 		with transaction.atomic():
@@ -18,9 +19,12 @@ class Command(BaseCommand):
 			library_id = options['library_id']
 			row = options['row']
 			column = options['column']
-			name = options['user']
-			wetlab_user = WetLabStaff.objects.get(first_name=name)
+			wetlab_user = get_wetlab_staff(options['user'])
 			user = wetlab_user.login_user
 			
-			self.stdout.write(f"{plate.name}\t{library_id}\t{row}\t{column}\t{wetlab_user.name()}")
-			plate.add_library(library_id, row, column, user)
+			if not options['delete']:
+				self.stdout.write(f"{plate.name}\t{library_id}\t{row}\t{column}\t{wetlab_user.name()}")
+				plate.add_library(library_id, row, column, user)
+			else:
+				element = CaptureLayout.objects.get(row=row, column=column, library__reich_lab_library_id=library_id, capture_batch=plate)
+				element.delete()
