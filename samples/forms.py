2331fa576a4d994db4266263e3515dae6f6a7e0d
Matthew Mah <mmah@broadinstitute.org>
2025-04-07 13:17:10 -0400

    sample form group label use level 1

--- samples/forms.py
+++ samples/forms.py
@@ -821,7 +821,7 @@ class SampleForm(UserModelForm):
 	
 	class Meta:
 		model = Sample
-		fields = ['reich_lab_id', 'external_id', 'special_restrictions', 'individual_id', 'collaborator', 'collection_keeper', 'excavator', 'skeletal_element', 'skeletal_element_category', 'skeletal_code', 'skeletal_code_renamed', 'sample_date', 'average_bp_date', 'date_fix_flag', 'dating_status', 'burial_subcode', 'accession_number', 'morphological_sex', 'morphological_age', 'morphological_age_range', 'approved_negative_results', 'approved_photo_sharing', 'outlier', 'notes', 'notes_2', 'loan_expiration_date', 'group_label_use_country', 'group_label_use_site', 'group_label_use_period', 'group_label_use_culture', 'periods', 'cultures']
+		fields = ['reich_lab_id', 'external_id', 'special_restrictions', 'individual_id', 'collaborator', 'collection_keeper', 'excavator', 'skeletal_element', 'skeletal_element_category', 'skeletal_code', 'skeletal_code_renamed', 'sample_date', 'average_bp_date', 'date_fix_flag', 'dating_status', 'burial_subcode', 'accession_number', 'morphological_sex', 'morphological_age', 'morphological_age_range', 'approved_negative_results', 'approved_photo_sharing', 'outlier', 'notes', 'notes_2', 'loan_expiration_date', 'group_label_use_country', 'group_label_use_level_1', 'group_label_use_site', 'group_label_use_period', 'group_label_use_culture', 'periods', 'cultures']
 		
 	def __init__(self, *args, **kwargs):
 		super(SampleForm, self).__init__(*args, **kwargs)
