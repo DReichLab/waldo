848a039973472789f78820684b04dbd8936233a6
Matthew Mah <mmah@broadinstitute.org>
2025-05-08 16:43:21 -0400

    allow removal of average_bp_date value during sample intake

--- samples/intake.py
+++ samples/intake.py
@@ -73,8 +73,7 @@ def sample_site_update(sample_file, user):
 			sample.skeletal_code = row.skeletal_code
 			sample.skeletal_element = row.skeletal_element
 			sample.sample_date = row.sample_date
-			if len(row.average_bp_date) > 0:
-				sample.average_bp_date = float(row.average_bp_date)
+			sample.average_bp_date = float(row.average_bp_date) if len(row.average_bp_date) > 0 else None
 			sample.date_fix_flag = row.date_fix_flag
 			sample.morphological_sex = row.morphological_sex
 			sample.morphological_age = row.morphological_age
