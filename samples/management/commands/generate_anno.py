a3c361c664ecd11cb1e8a024bd171a6d03e877c2
Matthew Mah <mmah@broadinstitute.org>
2025-06-23 14:17:36 -0400

    Greg reworked input file to explicitly mark external ID samples

--- samples/management/commands/generate_anno.py
+++ samples/management/commands/generate_anno.py
@@ -13,18 +13,21 @@ class Command(BaseCommand):
 		
 	def handle(self, *args, **options):
 		sample_ids_file = options['ids']
-		sample_ids = [line.rstrip('\n') for line in open(sample_ids_file, 'r')]
-
-		for sample_id in sample_ids:
-			try:
-				if re.fullmatch(SID_IID_REGEX, sample_id):
-					sample_id = sample_id[1:]
-				sample = Sample.objects.filter(control__length=0).get(reich_lab_id=int(sample_id))
-				output_id = f'S{sample.reich_lab_id}'
-			except (Sample.DoesNotExist, ValueError):
-				sample = Sample.objects.get(external_id=sample_id)
-				output_id = sample_id
-			#except Exception as e:
-			#	print('Failure for {}'.format(sample_id, file=sys.stderr)
-			#	raise e
-			self.stdout.write('\t'.join([output_id] + sample_anno(sample)))
+		
+		# read in file, two columns, tab-delimited
+		# ID	external(1) or blank
+		with open(sample_ids_file, 'r') as f:
+			for line in f:
+				fields = re.split('\t|\n', line)
+				sample_id = fields[0]
+				is_external = (fields[1] == '1')
+		
+				if is_external:
+					sample = Sample.objects.get(external_id=sample_id)
+					output_id = sample_id
+				else: # internal
+					match = re.fullmatch(SID_IID_REGEX, sample_id)
+					sample_id = int(match.groupdict()['sample'])
+					sample = Sample.objects.get(reich_lab_id=sample_id, control__length=0)
+					output_id = f'S{sample.reich_lab_id}'
+				self.stdout.write('\t'.join([output_id] + sample_anno(sample)))
