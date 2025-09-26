b8097276578ad04da24b4ae1d187e86ed245b2a9
Matthew Mah <mmah@broadinstitute.org>
2025-09-26 17:54:20 -0400

    anno type fix, sort by id by default

--- sequencing_run/management/commands/anno_file_generate.py
+++ sequencing_run/management/commands/anno_file_generate.py
@@ -20,7 +20,7 @@ class Command(BaseCommand):
 	def handle(self, *args, **options):
 		with transaction.atomic():
 			filter_threshold = options['filter']
-			entries = GeneticAnalysis.objects.filter(assessment__sort_order__gte=filter_threshold)
+			entries = GeneticAnalysis.objects.filter(assessment__sort_order__gte=filter_threshold).order_by('id')
 			if options['ids']:
 				entries = entries.filter(genetic_id__in=options['ids'])
 			
