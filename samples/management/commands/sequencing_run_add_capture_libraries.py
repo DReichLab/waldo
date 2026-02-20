0e6fb2848aca472adebbdb69512a6077029e231a
Matthew Mah <mmah@broadinstitute.org>
2026-02-20 18:28:24 -0500

    flexible user when adding to sequencing run

--- samples/management/commands/sequencing_run_add_capture_libraries.py
+++ samples/management/commands/sequencing_run_add_capture_libraries.py
@@ -1,6 +1,6 @@
 from django.core.management.base import BaseCommand, CommandError
 from django.db import transaction
-from samples.models import CaptureOrShotgunPlate, SequencingRun, CaptureLayout, SequencedLibrary, PCR_NEGATIVE, WetLabStaff
+from samples.models import CaptureOrShotgunPlate, SequencingRun, CaptureLayout, SequencedLibrary, PCR_NEGATIVE, WetLabStaff, get_wetlab_staff
 
 import re
 
@@ -10,7 +10,7 @@ class Command(BaseCommand):
 	def add_arguments(self, parser):
 		parser.add_argument("--capture_name", required=True, help='Source for indexed libraries to add to sequencing run')
 		parser.add_argument("--sequencing_run", required=True, help='Sequencing run destination')
-		parser.add_argument('--user', required=True, help='Wetlab user first name')
+		parser.add_argument('--user', required=True, help='Wetlab name or username')
 		parser.add_argument('--create', action='store_true', help='Create sequencing run object')
 		parser.add_argument('library_positions', help='File with two columns: library_id and position')
 		parser.add_argument('-i', '--ignore_positions', help='Load libraries from capture based on library id only. If library is not unique, command will fail.', action='store_true')
@@ -24,8 +24,7 @@ class Command(BaseCommand):
 			else:
 				sequencing_run = SequencingRun.objects.get(name=options['sequencing_run'])
 			
-			name = options['user']
-			wetlab_user = WetLabStaff.objects.get(first_name=name)
+			wetlab_user = get_wetlab_staff(options['user'])
 			user = wetlab_user.login_user
 
 			with open(options['library_positions']) as f:
